b2e8fc23fe37bedf66ff9de6f907c167c3d30da7
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-10-17 18:31:25 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -3,12 +3,10 @@ const allowedOrigins = ['https://resorse-hub-client.onrender.com'];
 exports.credentials = (req, res, next) =>{
     const origin = req.headers.origin;
     console.log(origin);
-    if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', 'https://resorse-hub-client.onrender.com');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         res.header('Access-Control-Allow-Credentials', true);
-    }
     next();
 }
 
