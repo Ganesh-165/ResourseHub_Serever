2efaf2f02ab26f2921ee75988c6bd96637219585
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-10-17 18:46:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const cors = require('cors');
 
 
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://resorse-hub-client.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.post('/sslc',async(req,res) => {
     try{
